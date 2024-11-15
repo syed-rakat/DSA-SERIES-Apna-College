62156487eba0c0f9f2c2a700c56e053de7074aed
syed-rakat <syedrafiulislam226@gmail.com>
2024-11-15 09:54:15 +0530

    this is my initial commit

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char str[]={'q','j','n','\0'};//constant pointers
+    int arr[]={1,2,3,4,5};
+    char str1[]="college";//string literals
+    cout<<arr<<endl;
+    cout<<str<<endl;
+    cout<<strlen(str)<<endl;
+    cout<<strlen(str1)<<endl;
+}
