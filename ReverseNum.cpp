a77da81548891cd861fb04a553ce8c5336a5efef
syed-rakat <syedrafiulislam226@gmail.com>
2024-11-17 21:29:49 +0530

    updated for leetcode solution for range

--- ReverseNum.cpp
+++ ReverseNum.cpp
@@ -8,6 +8,10 @@ int main()
     while(n!=0)
     {
         int digit=n%10;
+        if(revnum>INT_MAX/10 || revnum<INT_MIN/10)
+        {
+            return 0;
+        }
         revnum=revnum*10+digit;
         n=n/10;
     }
