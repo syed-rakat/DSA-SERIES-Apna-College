13ba299eec183f5c96e7f37fe9e2cc7e7848ffb0
syed-rakat <syedrafiulislam226@gmail.com>
2024-11-21 09:44:24 +0530

    reverse an array using linear search

---
+++ reversearr.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[]={1,3,5,7,18,23};
+    int size=6;
+    for(int i=size-1;i>=0;i--)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
